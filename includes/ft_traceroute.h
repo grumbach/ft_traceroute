7b95135a743d7fe123035573b27f53f8016e951c
grumbach <agrumbac@student.42.fr>
2018-12-12 05:49:57 +0000

    [ADD] And so it begins...

---
+++ includes/ft_traceroute.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/04 18:05:58 by agrumbac          #+#    #+#             */
+/*   Updated: 2018/11/04 18:33:45 by agrumbac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TRACEROUTE_H
+# define FT_TRACEROUTE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define __unused __attribute__((unused))
+
+#endif
